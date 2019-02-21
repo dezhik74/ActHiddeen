96e9d6e739149261ddd638e70fc9eb9f7304afa0
dezhik74 <dezhik@yandex.ru>
2019-02-21 22:10:14 +0300

    0.3.1 Список объектов с актами готов. Надо исправить модель с точки зрения названий, ввести несколько типов актов.

--- app/acts/hiddenactsbase/views.py
+++ app/acts/hiddenactsbase/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from .models import *
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 def act_list (request):
-    for act_set in HASet.objects.order_by("id"):
-        print(HAStandAlone.objects.filter(ha_set=act_set).order_by('id'))
-    return render (request,'hiddenactsbase/index.html',context = {'objects_acts' : HASet.objects.order_by("id")})
+ #   for act_set in HASet.objects.order_by("id"):
+ #       print(HAStandAlone.objects.filter(ha_set=act_set).order_by('id'))
+    return render (request,'hiddenactsbase/index.html',context = {'objects_acts' : HASet.objects.order_by("-id"),
+                                                                    'acts':HAStandAlone.objects.order_by('id')})
