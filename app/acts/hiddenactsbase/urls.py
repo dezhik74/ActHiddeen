f48db0fae0e097b4f6fc9ea3bcfaa97021eed9a2
dezhik74 <dezhik@yandex.ru>
2019-03-18 23:30:15 +0300

    0.5.1 Работаем над подготовкой вордовского файла

--- app/acts/hiddenactsbase/urls.py
+++ app/acts/hiddenactsbase/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('see-<int:pk>/', ObjectDetail.as_view(), name='object_detail_url'),
     path('create_hidden_to_end-<int:pk>/', create_hidden_act_to_end, name='create_hidden_act_to_end'),
     path('delete-<int:pk_obj>-<int:pk_act>', delete_hidden_act, name='delete_hidden_act'),
-    path('copy-<int:pk>', copy_object, name='copy_object')
+    path('copy-<int:pk>', copy_object, name='copy_object'),
+    path('obj_delete-<int:pk>', delete_object, name='delete_object'),
+    path('make_word-<int:pk>', make_word_file, name='make_word_file')
     ]
--- app/acts/hiddenactsbase/views.py
+++ app/acts/hiddenactsbase/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render, get_object_or_404, HttpResponse, redirect
-from django.views.generic import View
-from .models import *
-from .forms import ObjectForm, HActISForm
 from django.forms import formset_factory
+from django.shortcuts import render, get_object_or_404, redirect, HttpResponse
+from django.views.generic import View
+
+from .forms import ObjectForm, HActISForm
+from .models import *
+import os
 
 
-class ObjectsList (View):
-    def get(self,request):
+class ObjectsList(View):
+    def get(self, request):
         objs = ObjectActs.objects.order_by("-id")
         return render(request, 'hiddenactsbase/index.html', context={'objects_acts': objs})
 
@@ -41,10 +43,12 @@ def update_obj(obj, cleaned_obj_data, cleaned_act_data):
     obj.save()
     return obj
 
+
 def create_hidden_act_to_end (request, pk):
     myobj = get_object_or_404(ObjectActs, pk=pk)
     myobj.acts.create()
     return redirect(myobj)
+
 
 def delete_hidden_act (request, pk_obj, pk_act):
     myobj = get_object_or_404(ObjectActs, pk=pk_obj)
@@ -55,8 +59,11 @@ def delete_hidden_act (request, pk_obj, pk_act):
 
 def copy_object (request, pk):
     myobj = get_object_or_404(ObjectActs, pk=pk)
+    s = myobj.address
+    if len(s) > 93:
+        s = s[0:93]
     newobj=ObjectActs.objects.create(
-        address = myobj.address,
+        address = 'Копия: ' + s,
         system_type = myobj.system_type,
         designer = myobj.designer,
         supervisor_engineer = myobj.supervisor_engineer,
@@ -66,19 +73,32 @@ def copy_object (request, pk):
         supervisor_engineer_decree = myobj.supervisor_engineer_decree,
         contractor_engineer_decree = myobj.contractor_engineer_decree
      )
-    # for act in myobj.acts.all():
-    #     newobj.acts.create(
-    #         act_number = act.act_number,
-    #         act_date = act.act_date,
-    #         presented_work = act.presented_work,
-    #         materials = act.materials,
-    #         permitted_work = act.permitted_work,
-    #         begin_date = act.begin_date,
-    #         end_date = act.end_date
-    #     )
-    #return redirect('objects_list_url')
+    for act in myobj.acts.all():
+        newobj.acts.create(
+            act_number = act.act_number,
+            act_date = act.act_date,
+            presented_work = act.presented_work,
+            materials = act.materials,
+            permitted_work = act.permitted_work,
+            begin_date = act.begin_date,
+            end_date = act.end_date
+        )
     return redirect(newobj)
 
+
+def delete_object(request, pk):
+    myobj = get_object_or_404(ObjectActs, pk=pk)
+    for act in myobj.acts.all():
+        act.delete()
+    myobj.delete()
+    return redirect('objects_list_url')
+
+
+def make_word_file(request, pk):
+    BASE_APP_DIR = os.path.dirname(os.path.abspath(__file__))
+    DYNAMIC_DIR =  os.path.join(BASE_APP_DIR,'dynamic')
+    W_TEMPLATE = os.path.join(BASE_APP_DIR,'HiddenActTemtplate2.docx')
+    return HttpResponse (str(pk)+' '+W_TEMPLATE)
 
 
 def objectedit (request, pk):
@@ -124,72 +144,4 @@ def objectedit (request, pk):
             'myobj': myobj,
             'object_form_set': object_form_set,
             'ha_form_set': ha_form_set})
-
-
-# class ObjectEdit(View):
-    # len(oa1.acts.all()) !!! длина списка актов скрытых. Если 0, то их нет !!!
-    # ИДЕЯ ДЛЯ соединения двух форм на одной странице
-    # ObjectFormSet = formset_factory(ObjectForm)
-    # HActISFormSet = formset_factory(HActISForm, extra=6)
-    # object_form_set = ObjectFormSet(prefix='objects')
-    # hact_is_form_set = HActISFormSet(prefix='hacts')
-    # print(object_form_set, hact_is_form_set)
-
-
-    # def get(self,request,pk):
-    #     pass
-
-    # def post(self,request,pk):
-    #     pass
-
-    # СТАРАЯ РЕАЛИЗАЦИЯ
-    # def get(self, request, pk):
-    #     myobj = get_object_or_404(ObjectActs, pk=pk)
-    #     ObjectFormSet = formset_factory(ObjectForm, extra=0)
-    #     object_form_set = ObjectFormSet (prefix='object_data', initial=[{
-    #         'address': myobj.address,
-    #         'system_type': myobj.system_type,
-    #         'designer': myobj.designer,
-    #         'supervisor_engineer': myobj.supervisor_engineer,
-    #         'contractor_engineer': myobj.contractor_engineer,
-    #         'project_number': myobj.project_number,
-    #         'exec_documents': myobj.exec_documents,
-    #         'supervisor_engineer_decree': myobj.supervisor_engineer_decree,
-    #         'contractor_engineer_decree': myobj.contractor_engineer_decree
-    #     }])
-    #     # acts_num=len(myobj.acts.all())
-    #     initial = []
-    #     for act in myobj.acts.all():
-    #         initial.append({
-    #             'act_number' : act.act_number,
-    #             'act_date' : act.act_date,
-    #             'presented_work' : act.presented_work,
-    #             'materials' : act.materials,
-    #             'permitted_work' : act.permitted_work,
-    #             'begin_date' : act.begin_date,
-    #             'end_date' : act.end_date
-    #         })
-    #     HActISFormSet = formset_factory(HActISForm, extra=0)
-    #     ha_form_set=HActISFormSet(prefix='hidden_acts', initial = initial)
-    #     return render(request, 'hiddenactsbase/object_edit.html', context={
-    #         'myobj': myobj,
-    #         'object_form_set': object_form_set,
-    #         'ha_form_set':ha_form_set})
-    #     # return HttpResponse(acts_num)
-    #
-    # def post(self,request,pk):
-    #     myobj = get_object_or_404(ObjectActs, pk=pk)
-    #     ObjectFormSet = formset_factory(ObjectForm, extra=0)
-    #     HActISFormSet = formset_factory(HActISForm, extra=0)
-    #     object_form_set = ObjectFormSet (request.POST, prefix='object_data')
-    #     ha_form_set = HActISFormSet(request.POST, prefix='hidden_acts')
-    #     i1= object_form_set.is_valid()
-    #     i2 = ha_form_set.is_valid()
-    #     if object_form_set.is_valid() and ha_form_set.is_valid():
-    #         print(object_form_set.cleaned_data)
-    #         print(ha_form_set.cleaned_data)
-    #         new_object_acts = myobj
-    #         return redirect(new_object_acts)
-    #     return render(request, 'hiddenactsbase/object_edit.html', context={'myobj': myobj, 'form': form})
-
 
