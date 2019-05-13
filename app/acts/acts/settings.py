5fa96dec802dbb1a263a40a896f136a423fed10a
dezhik74 <dezhik@yandex.ru>
2019-05-13 23:22:35 +0300

    0.6.9.7 Докер настроен!

--- app/acts/acts/settings.py
+++ app/acts/acts/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_aehzl#8dx8dsz=o5@-v7%pp5qn076oywr^cxq%av)lsux9$63'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '192.168.56.107']
+ALLOWED_HOSTS = ['127.0.0.1', '192.168.56.107', 'localhost']
 
 
 # Application definition
@@ -76,23 +76,23 @@ WSGI_APPLICATION = 'acts.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'django_db',
-#         'USER': 'user2',
-#         'PASSWORD': 'pww2',
-#         'HOST': '127.0.0.1',
-#         'PORT': '5432',
-#     }
-# }
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': 'postgres',
+         'USER': 'postgres',
+         'PASSWORD': 'pww2',
+         'HOST': 'db',
+         'PORT': '5432',
+     }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -131,3 +131,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR + '/static'
--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -4,8 +4,8 @@ echo "PREPARE DATABASE..."
 
 cd ./app/acts
 
-# echo "Collect static files..."
-# python manage.py collectstatic --noinput
+echo "Collect static files..."
+python manage.py collectstatic --noinput
 
 # echo "Получение дампа данных только при первом запуске контейнера"
 # python manage.py loaddata datadump.json
@@ -14,9 +14,9 @@ cd ./app/acts
 # python manage.py migrate
 
 echo "Starting server..."
-echo "Django development server..."
-python manage.py runserver 0.0.0.0:8000
+# echo "Django development server..."
+# python manage.py runserver 0.0.0.0:8000
 
-# echo "Gunicorn server"
-# gunicorn -b 0.0.0.0:8000 acts.wsgi:application
+echo "Gunicorn server"
+gunicorn -b 0.0.0.0:8000 acts.wsgi:application
 
